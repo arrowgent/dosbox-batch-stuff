f4e75ad4d8396dd8b6cb0b7203f58a683a29899f
arrowgent <57545549+arrowgent@users.noreply.github.com>
2025-04-23 09:13:10 -0700

    herhex files

---
+++ loaders/heretic_hexen/herhexL.bat
@@ -0,0 +1,88 @@
+:MENU
+cls
+@echo off
+ECHO [1;33m[7;20H ีออออออออออออออออออออออออออออออออออธ
+ECHO [1;33m[8;20H ณ[8;30H[0;34m- Heretic & HEXEN -[1;33m[8;55H ณ
+ECHO [1;33m[9;20H ิออออออออออออออออออออออออออออออออออพ
+ECHO [0m[5;36m[10;20H ๐
+ECHO [0;31m[11;20H 1 - Heretic Shareware v1.0 (1994)
+ECHO [0;31m[12;20H 2 - Heretic Shareware v1.2 (1995)
+ECHO [0;31m[13;20H 3 - Heretic v1.0 (1994)
+ECHO [0;31m[14;20H 4 - Heretic v1.2 (1995)
+ECHO [0;31m[15;20H 5 - Heretic v1.3 Shadow of the Serpent Riders (1996)
+ECHO [0;31m[16;20H 6 - HEXEN Demo v1.0 (1995)
+ECHO [0;31m[17;20H 7 - HEXEN Beyond Heretic v1.1 (1995)
+ECHO [0;31m[18;20H 8 - HEXEN Deathkings of the Dark Citadel (1996)
+ECHO [31;40m[19;20H 0 - EXIT - X
+ECHO [5;36m[20;20H ๐[20H[0m[1;30m
+@echo on
+@CHOICE /C:1234567890X /N  Select: 1, 2, 3, 4, 5, 6, 7, 8, 0 or X
+@echo off
+echo [0m
+IF ERRORLEVEL==9 GOTO DONE
+IF ERRORLEVEL==8 GOTO 8HXDD
+IF ERRORLEVEL==7 GOTO 7HX11
+IF ERRORLEVEL==6 GOTO 6HXDM
+IF ERRORLEVEL==5 GOTO 5HR13
+IF ERRORLEVEL==4 GOTO 4HR12
+IF ERRORLEVEL==3 GOTO 3HR10
+IF ERRORLEVEL==2 GOTO 2HSW2
+IF ERRORLEVEL==1 GOTO 1HSW1
+IF ERRORLEVEL==0 GOTO DONE
+:1HSW1
+HSW10.EXE
+echo [23H[0m[1;30m
+pause [24H[0m
+GOTO MENU
+:2HSW2
+HSW12.EXE
+echo [23H[0m[1;30m
+pause [24H[0m
+GOTO MENU
+:3HR10
+HERETIC.EXE
+echo [23H[0m[1;30m
+pause [24H[0m
+GOTO MENU
+:4HR12
+HERETIC.EXE
+echo [23H[0m[1;30m
+pause [24H[0m
+GOTO MENU
+:5HR13
+HERETIC.EXE
+echo [23H[0m[1;30m
+pause [24H[0m
+GOTO MENU
+:6HXDM
+DEMO.EXE
+echo [23H[0m[1;30m
+pause [24H[0m
+GOTO MENU
+:7HX11
+HEXEN.EXE
+echo [23H[0m[1;30m
+pause [24H[0m
+GOTO MENU
+:8HXDD
+HEXDD.EXE
+echo [23H[0m[1;30m
+pause [24H[0m
+GOTO MENU
+:DONE
+cls
+@echo off
+ECHO [1;33m[7;20H ีออออออออออออออออออออออออออออออออออธ
+ECHO [1;33m[8;20H ณ[8;30H[0;34m- Heretic & HEXEN -[1;33m[8;55H ณ
+ECHO [1;33m[9;20H ิออออออออออออออออออออออออออออออออออพ
+ECHO [0m[5;36m[10;20H ๐
+ECHO [0;31m[11;20H 1 - Heretic Shareware v1.0 (1994)
+ECHO [0;31m[12;20H 2 - Heretic Shareware v1.2 (1995)
+ECHO [0;31m[13;20H 3 - Heretic v1.0 (1994)
+ECHO [0;31m[14;20H 4 - Heretic v1.2 (1995)
+ECHO [0;31m[15;20H 5 - Heretic v1.3 Shadow of the Serpent Riders (1996)
+ECHO [0;31m[16;20H 6 - HEXEN Demo v1.0 (1995)
+ECHO [0;31m[17;20H 7 - HEXEN Beyond Heretic v1.1 (1995)
+ECHO [0;31m[18;20H 8 - HEXEN Deathkings of the Dark Citadel (1996)
+ECHO [31;40m[19;20H 0 - EXIT - X
+ECHO [5;36m[20;20H ๐[20H[0m[1;30m
