3c5253a960290273b5560f5620ef975e8a1904f6
arrowgent <57545549+arrowgent@users.noreply.github.com>
2025-04-23 08:37:50 -0700

    doom files

---
+++ loaders/doom/doomL.bat
@@ -0,0 +1,31 @@
+cls
+@echo off
+ECHO [1;33m[7;20H ีออออออออออออออออออออออออออออออออออธ
+ECHO [1;33m[8;20H ณ[8;30H[0;34m- id Software -[1;33m[8;55H ณ
+ECHO [1;33m[9;20H ิออออออออออออออออออออออออออออออออออพ
+ECHO [0m[5;36m[10;20H ๐
+ECHO [0;31m[11;20H 1 - Wolfenstein 3d (1992)
+ECHO [0;31m[12;20H 2 - Doom Shareware v1.0 (1993)
+ECHO [0;31m[13;20H 3 - Doom Shareware v1.666 (1993)
+ECHO [0;31m[14;20H 4 - Doom Shareware v1.9 (1993)
+ECHO [0;31m[15;20H 5 - Doom (1993)
+ECHO [0;31m[16;20H 6 - Doom - Ultimate (1995)
+ECHO [0;31m[17;20H 7 - Doom ][ (1994)
+ECHO [31;40m[18;20H 0 - EXIT - X
+ECHO [5;36m[19;20H ๐[20H[0m[1;30m
+pause
+cls
+@echo off
+ECHO [1;33m[7;20H ีออออออออออออออออออออออออออออออออออธ
+ECHO [1;33m[8;20H ณ[8;30H[0;34m- id Software -[1;33m[8;55H ณ
+ECHO [1;33m[9;20H ิออออออออออออออออออออออออออออออออออพ
+ECHO [0m[5;36m[10;20H ๐
+ECHO [0;31m[11;20H 1 - Wolfenstein 3d (1992)
+ECHO [0;31m[12;20H 2 - Doom Shareware v1.0 (1993)
+ECHO [0;31m[13;20H 3 - Doom Shareware v1.666 (1993)
+ECHO [0;31m[14;20H 4 - Doom Shareware v1.9 (1993)
+ECHO [0;31m[15;20H 5 - Doom (1993)
+ECHO [0;31m[16;20H 6 - Doom - Ultimate (1995)
+ECHO [0;31m[17;20H 7 - Doom ][ (1994)
+ECHO [31;40m[18;20H 0 - EXIT - X
+ECHO [5;36m[19;20H ๐[20H[0m[1;30m
